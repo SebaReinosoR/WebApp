f3cce2a2f07f30c669173751a48aa6d7f08a0208
SebaReinosoR <sebastian.reinoso.r@mail.pucv.cl>
2023-10-09 08:01:36 -0300

    front add 1.1
    se realiza en TS la gestión de elementos mostrados por el la selección "select"

--- src/app/add-elemento/add-elemento.component.ts
+++ src/app/add-elemento/add-elemento.component.ts
@@ -1,27 +1,77 @@
-import { Component } from '@angular/core';
+import { Component , AfterViewInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-add-elemento',
   templateUrl: './add-elemento.component.html',
   styleUrls: ['./add-elemento.component.scss']
 })
-export class AddElementoComponent {
+export class AddElementoComponent implements AfterViewInit{
 
 
-  constructor(){
-
-    let arreglo : string[] = [
-
+    arreglo: string[] = [
       "Temas",
       "Encargados",
       "Codigos",
-      "BP",
-      "documentacion",
-      "publicacion",
-      "Subtema"
+      "Buenas Practicas",
+      "Documentación",
+      "Publicación",
+      "Subtema",
     ];
 
+    // Llamada a la función para cargar elementos en ngAfterViewInit
+    ngAfterViewInit() {
+      this.cargar_elementos();
+    }
+
+    // Función para cargar elementos en el campo <select>
+    cargar_elementos() {
+      // Ordena el Array Alfabéticamente
+      this.arreglo.sort();
+
+      this.addOptions("elementos", this.arreglo);
+    }
+
+    // Rutina para agregar opciones a un <select>
+    addOptions(domElement: string, arreglo: string[]) {
+      var select = document.querySelector(`select[name="${domElement}"]`) as HTMLSelectElement;
+
+      for (let value of arreglo) {
+        var option = document.createElement("option");
+        option.text = value;
+        select.add(option);
+      }
+
+    }
+
+    opcionSeleccionada: string = '0'; // Opción predeterminada
+    mostrarTemas: boolean = false;
+    mostrarEncargados: boolean = false;
+    mostrarCodigos: boolean = false;
+    mostrarBuenasPracticas: boolean = false;
+    mostrarDocumentacion: boolean = false;
+    mostrarPublicacion: boolean = false;
+    mostrarSubtema: boolean = false;
+
+    onOptionChange(event: any) {
+      this.opcionSeleccionada = event.target.value;
+    
+        // Establece las condiciones para mostrar/ocultar campos según la opción seleccionada
+        this.mostrarTemas = this.opcionSeleccionada === 'Temas';
+        this.mostrarEncargados= this.opcionSeleccionada === 'Encargados';
+        this.mostrarCodigos = this.opcionSeleccionada === 'Codigos';
+        this.mostrarBuenasPracticas = this.opcionSeleccionada === 'Buenas Practicas';
+        this.mostrarDocumentacion = this.opcionSeleccionada === 'Documentación';
+        this.mostrarPublicacion = this.opcionSeleccionada === 'Publicación';
+        this.mostrarSubtema = this.opcionSeleccionada === 'Subtema';
+        // Define condiciones para otros campos aquí
+      };
+  
+    
   }
 
 
-}
+
+
+
+
